f9e2424c2bf39e1eab2fdf0aacbfda5386be6d48
root <root@master.e2hdelgek01ubazewgv0fpsozb.ax.internal.cloudapp.net>
2019-03-01 09:16:29 +0000

    Second commit

--- forms.py
+++ forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 class LoginForm(forms.Form):
    username = forms.CharField(max_length = 100)
-   password = forms.CharField(widget = forms.PasswordInput())
+   password = forms.CharField(max_length = 100)
+
--- views.py
+++ views.py
@@ -4,19 +4,28 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 #from django.views.generic import TemplateView
 from myapp.forms import LoginForm
-
+import subprocess
+from subprocess import call, check_call
+#call('echo "I like potatos"', shell=True)
 #class HomePageView(TemplateView):
 def login(request):
-    username = "NOT lOGGED IN"
+    username = "PACKAGE INSTALLED"
+    password = "ALREADY"
+    #call('ansible-playbook sahil.yml -l $username', shell=True)
     if request.method == "POST":
         #Get the posted form
         MyLoginForm = LoginForm(request.POST)
         if MyLoginForm.is_valid():
             username = MyLoginForm.cleaned_data['username']
+            password = MyLoginForm.cleaned_data['password']
+            call("echo " + password + "> pack", shell=True)
+           # call(["echo", %k],shell=True)
+            #call('echo MyLoginForm.cleaned_data['username']',shell=True)
+            call("ansible-playbook sahil.yml -l " + username, shell=True)
     else:
         MyLoginForm = Loginform()
 		
-    return render(request, 'loggedin.html', {"username" : username})
+    return render(request, 'loggedin.html', {"username" : username,"password" : password})
  #   def get(self, request, **kwargs):
   #      return render(request, 'hello.html', context=None)
 
